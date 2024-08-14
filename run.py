1ac0210e5c8eae3ea2ef1ed362cdc6233a25230a
AJ Gambino <66279789+ajGambino@users.noreply.github.com>
2024-08-14 12:00:31 -0400

    update cors

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+from flask import Flask
 from flask_cors import CORS
 from app import app
 
 
-CORS(app, resources={r"/*": {"origins": "*"}})
-if __name__ == '__main__':
-    app.run(debug=True)
+CORS(app, resources={r"/*": {"origins": "https://theresurrection.vercel.app"}})
+
+# below is for local development
+# if __name__ == '__main__':
+#     app.run(debug=True)
